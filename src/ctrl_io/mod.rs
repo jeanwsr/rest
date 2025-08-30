7be3ccb1b1ec1f434f53c689aafc3d38b1c396e2
DeltaH <12386037+delta_h@user.noreply.gitee.com>
2025-08-30 12:14:30 +0000

    Add 1D RRS-PBC

--- src/ctrl_io/mod.rs
+++ src/ctrl_io/mod.rs
@@ -1436,6 +1436,62 @@ impl InputKeywords {
                     serde_json::Value::String(tmp_str) => {tmp_str.clone()},
                     other => {String::from("none")},
                 };
+                //for rrs_pbc
+                tmp_geomcell.rrs_pbc = match tmp_geom.get("rrs_pbc").unwrap_or(&serde_json::Value::Null) {
+                    serde_json::Value::Bool(tmp_str) => {*tmp_str},
+                    other => {false},
+                };
+                tmp_geomcell.num_units = match tmp_geom.get("num_units").unwrap_or(&serde_json::Value::Null) {
+                    serde_json::Value::String(tmp_str) => {tmp_str.to_lowercase().parse().unwrap_or(1_usize)},
+                    serde_json::Value::Number(tmp_num) => {tmp_num.as_i64().unwrap_or(1) as usize},
+                    other => {1_usize}
+                };
+                tmp_geomcell.pbc_dim = match tmp_geom.get("pbc_dim").unwrap_or(&serde_json::Value::Null) {
+                    serde_json::Value::String(tmp_str) => {tmp_str.to_lowercase().parse().unwrap_or(1_usize)},
+                    serde_json::Value::Number(tmp_num) => {tmp_num.as_i64().unwrap_or(1) as usize},
+                    other => {1_usize}
+                };
+                tmp_geomcell.rrs_pbc_vec = match tmp_geom.get("rrs_pbc_vec").unwrap_or(&serde_json::Value::Null) {
+                    serde_json::Value::Array(tmp_op) => {
+                        let tmp_vec: Vec<f64> = tmp_op.iter().map(|x| {
+                            match x {
+                                serde_json::Value::String(tmp_str) => {tmp_str.parse().unwrap_or(0.0)},
+                                serde_json::Value::Number(tmp_num) => {tmp_num.as_f64().unwrap_or(0.0)},
+                                other => {0.0},
+                            }
+                        }).collect::<Vec<f64>>();
+                        MatrixFull::from_vec([tmp_geomcell.pbc_dim,3],tmp_vec).unwrap()
+                    },
+                    other => {MatrixFull::empty()},
+                };
+                tmp_geomcell.k_points = match tmp_geom.get("k_points").unwrap_or(&serde_json::Value::Null) {
+                    serde_json::Value::String(tmp_str) => {tmp_str.to_lowercase().parse().unwrap_or(1_usize)},
+                    serde_json::Value::Number(tmp_num) => {tmp_num.as_i64().unwrap_or(1) as usize},
+                    other => {1_usize}
+                };
+                tmp_geomcell.unit_cell_elem = match tmp_geom.get("unit_cell_elem").unwrap_or(&serde_json::Value::Null) {
+                    serde_json::Value::String(tmp_op) => {vec![tmp_op.to_string()]},
+                    serde_json::Value::Array(tmp_op) => {
+                        let mut tmp_vec: Vec<String> = vec![];
+                        tmp_op.iter().for_each(|x| {
+                            let op_type = x.to_string();
+                            let string_len = op_type.len();
+                            tmp_vec.push(op_type[1..string_len-1].to_string())
+                        });
+                        tmp_vec
+                    },
+                    other => {vec![]},
+                };
+                //rrs_pbc check
+                if tmp_geomcell.rrs_pbc {
+                    if tmp_geomcell.pbc_dim != 1_usize {
+                        panic!("I am sorry but 2D and 3D RRS-PBC methods are not yet implemented");
+                    };
+                    if tmp_geomcell.num_units % 2 == 0 {
+                        panic!("1D RRS-PBC method requires num_units to be odd");
+                    };
+                };
+                //rrs_pbc end
                 let tmp_unit = match tmp_geom.get("unit").unwrap_or(&serde_json::Value::Null) {
                     serde_json::Value::String(tmp_str) => {tmp_str.to_lowercase()},
                     other => {String::from("angstrom")},
