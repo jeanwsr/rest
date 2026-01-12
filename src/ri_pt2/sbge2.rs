a0017e4caa97af3d725138c92daaea97bedfec9f
Lingyue Yu <lyyu23@m.fudan.edu.cn>
2026-01-13 03:07:12 +0800

    feature: RO-sBGE2

--- src/ri_pt2/sbge2.rs
+++ src/ri_pt2/sbge2.rs
@@ -1239,8 +1239,12 @@ pub fn open_shell_sbge2_rayon_mpi(scf_data: &crate::scf_io::SCF, mpi_operator:&O
                 if i_spin_1 == i_spin_2 {
 
                     let i_spin = i_spin_1;
-                    let eigenvector = scf_data.eigenvectors.get(i_spin).unwrap();
-                    let eigenvalues = scf_data.eigenvalues.get(i_spin).unwrap();
+                    let eigenvector = match scf_data.scftype { SCFType::RHF | SCFType::UHF => scf_data.eigenvectors.get(i_spin).unwrap(),
+                        SCFType::ROHF => &scf_data.semi_eigenvectors.as_ref().unwrap()[i_spin]
+                    };
+                    let eigenvalues = match scf_data.scftype { SCFType::RHF | SCFType::UHF => scf_data.eigenvalues.get(i_spin).unwrap(),
+                        SCFType::ROHF => &scf_data.semi_eigenvalues.as_ref().unwrap()[i_spin]
+                    };
                     let occupation = scf_data.occupation.get(i_spin).unwrap();
 
                     let homo = scf_data.homo[i_spin].clone();
@@ -1343,8 +1347,12 @@ pub fn open_shell_sbge2_rayon_mpi(scf_data: &crate::scf_io::SCF, mpi_operator:&O
 
 
                 } else {
-                    let eigenvector_1 = scf_data.eigenvectors.get(i_spin_1).unwrap();
-                    let eigenvalues_1 = scf_data.eigenvalues.get(i_spin_1).unwrap();
+                    let eigenvector_1 = match scf_data.scftype { SCFType::RHF | SCFType::UHF => scf_data.eigenvectors.get(i_spin_1).unwrap(),
+                        SCFType::ROHF => &scf_data.semi_eigenvectors.as_ref().unwrap()[i_spin_1]
+                    };
+                    let eigenvalues_1 = match scf_data.scftype { SCFType::RHF | SCFType::UHF => scf_data.eigenvalues.get(i_spin_1).unwrap(),
+                        SCFType::ROHF => &scf_data.semi_eigenvalues.as_ref().unwrap()[i_spin_1] 
+                    };
                     let occupation_1 = scf_data.occupation.get(i_spin_1).unwrap();
                     let homo_1 = scf_data.homo.get(i_spin_1).unwrap().clone();
                     let lumo_1 = scf_data.lumo.get(i_spin_1).unwrap().clone();
@@ -1353,8 +1361,12 @@ pub fn open_shell_sbge2_rayon_mpi(scf_data: &crate::scf_io::SCF, mpi_operator:&O
                     let (rimo_1, vir_range, occ_range) = &ri3mo_vec[i_spin_1];
                     let lumo_min = vir_range.start;
 
-                    let eigenvector_2 = scf_data.eigenvectors.get(i_spin_2).unwrap();
-                    let eigenvalues_2 = scf_data.eigenvalues.get(i_spin_2).unwrap();
+                    let eigenvector_2 = match scf_data.scftype { SCFType::RHF | SCFType::UHF => scf_data.eigenvectors.get(i_spin_2).unwrap(),
+                        SCFType::ROHF => &scf_data.semi_eigenvectors.as_ref().unwrap()[i_spin_2]
+                    };
+                    let eigenvalues_2 = match scf_data.scftype { SCFType::RHF | SCFType::UHF => scf_data.eigenvalues.get(i_spin_2).unwrap(),
+                        SCFType::ROHF => &scf_data.semi_eigenvalues.as_ref().unwrap()[i_spin_2]
+                    };
                     let occupation_2 = scf_data.occupation.get(i_spin_2).unwrap();
                     let homo_2 = scf_data.homo.get(i_spin_2).unwrap().clone();
                     let lumo_2 = scf_data.lumo.get(i_spin_2).unwrap().clone();
