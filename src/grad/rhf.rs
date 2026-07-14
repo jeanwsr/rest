bc29a0022fece9c359350f7037cc493b395cb785
ajz34 <ajz34@outlook.com>
2026-07-14 14:37:47 +0800

    grad/rijk: add more intutive message when unwraping rimatr as option

--- src/grad/rhf.rs
+++ src/grad/rhf.rs
@@ -224,7 +224,8 @@ impl RIRHFGradient<'_> {
 
         // eigen-decomposed ERI
         let ederi_utp = {
-            let tsr = self.scf_data.rimatr.as_ref().unwrap();
+            let msg = "Decomposed ERI not found, possibly due to insufficient memory. We do not support ri-direct gradient calculation.";
+            let tsr = self.scf_data.rimatr.as_ref().expect(msg);
             rt::asarray((&tsr.0.data, tsr.0.size, &device))
         };
         let naux = ederi_utp.shape()[1];
